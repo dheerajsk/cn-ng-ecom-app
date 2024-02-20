1ec94e0c9595a634b5e0773bb64f8780c54c0577
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-02-20 16:40:03 +0530

    creating components

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
