6bcf95f4f8cca3faaa39db35f88a17eeb8a2511c
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-02-20 17:44:17 +0530

    ngclass,ngstyles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  fontSize = 24;
+}
--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'menu-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css',
 })
-export class MenuBarComponent {}
+export class MenuBarComponent {
+  color: string = 'red';
+
+  getStyle() {
+    return {
+      'background-color': 'red',
+    };
+  }
+}
