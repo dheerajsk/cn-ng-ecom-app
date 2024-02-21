6691c316f5273ae5a512d469b504c515b734f170
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-02-21 14:32:22 +0530

    carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderCarouselComponent } from './header-carousel/header-carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MenuBarComponent, HomeComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    MenuBarComponent,
+    HomeComponent,
+    HeaderCarouselComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/header-carousel/header-carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-carousel.component.html',
+  styleUrl: './header-carousel.component.css',
+})
+export class HeaderCarouselComponent {}
