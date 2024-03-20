c49e03eb7f5adc5233effaf288c75a4b04c920ac
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2024-03-20 08:06:57 +0530

    Liefcycle hooks 3

--- src/app/home/product-rating/product-rating.component.ts
+++ src/app/home/product-rating/product-rating.component.ts
@@ -13,6 +13,10 @@ export class ProductRatingComponent {
   // 3.2 3 full, 1 half, 1 empty.
   // 4
 
+  ngOnChanges() {
+    this.createStars();
+  }
+
   createStars() {
     // 1. Get full stars.
     const fullStars = Math.floor(this.rating); // 4 // 3
@@ -22,7 +26,7 @@ export class ProductRatingComponent {
     const emptyStars = 5 - fullStars - (halfStars ? 1 : 0); // = 1
     this.stars = [
       ...Array(fullStars).fill({ filled: true }),
-      ...Array(halfStars ? [{ filled: 'half' }] : []),
+      ...Array(halfStars ? { filled: 'half' } : {}),
       ...Array(emptyStars).fill({ filled: false }),
     ];
   }
